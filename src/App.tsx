e452276a39a762e3816529af84871197cf95364b
mochi <git@daifuku.moe>
2025-03-14 00:42:17 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { KitsuSearch } from "./components/KitsuSearch/KitsuSearch";
+
+function App() {
+  return (
+    <>
+      <KitsuSearch />
+    </>
+  );
+}
+
+export default App;
